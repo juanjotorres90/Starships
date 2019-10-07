875ab7a18e3aeac09fc3f83a60e03f3e3b8d21ab
Juanjo Torres <juanjotorres90@gmail.com>
2019-10-07 13:53:52 +0200

    Initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShipsComponent } from './ships/ships.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/ships', pathMatch: 'full' },
+  { path: 'ships', component: ShipsComponent }
+  // { path: 'list', component: ListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'starships';
+
+  ngOnInit() {
+    $(function() {
+      $('[data-toggle="popover"]').popover();
+    });
+  }
 }
